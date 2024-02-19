85c5adb65965c0fbcfefd6ea2306b531bc6a66d0
AleemIlyas <aleemempire215@gmail.com>
2024-02-19 23:18:04 +0500

    change home page in package json

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="https://aleemilyas.github.io/Portfolio/">
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
