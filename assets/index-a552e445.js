37a26e9f4ecd3a254dbb17824e0b326b07af09a8
AleemIlyas <AleemIlyas@users.noreply.github.com>
2024-02-20 15:02:23 +0000

    deploy: 4ed437e08f501488a89f56f3d7a5982218cb36dd

--- assets/index-a552e445.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-d95f65a9.js";import o from"./Layout-a9e09a4d.js";function i(){return r.jsx(o,{children:r.jsx("h1",{children:"Home"})})}export{i as default};
---
+++ assets/index-aef49b35.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-588ddd2d.js";import o from"./Layout-685b1776.js";function i(){return r.jsx(o,{children:r.jsx("h1",{children:"Home"})})}export{i as default};
