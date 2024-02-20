567a4f342d121e5b1ffb2c7a1f2d9052278600d3
AleemIlyas <AleemIlyas@users.noreply.github.com>
2024-02-20 14:39:29 +0000

    deploy: efaa1c0ae6382f1310ba6761551b86bfb7671851

--- assets/index-1ae0f572.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-2b3c62ca.js";import o from"./Layout-1ff465ba.js";function i(){return r.jsx(o,{children:r.jsx("h1",{children:"Home"})})}export{i as default};
---
+++ assets/index-63fb7853.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-5213d9d2.js";import o from"./Layout-62b6c6a1.js";function i(){return r.jsx(o,{children:r.jsx("h1",{children:"Home"})})}export{i as default};
