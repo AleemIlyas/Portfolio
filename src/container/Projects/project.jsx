612e3b6c9ed0d24b975a49d3c0be040eeb5fd08b
AleemIlyas <aleemempire215@gmail.com>
2024-02-20 19:35:26 +0500

    updated url with production

--- src/container/Projects/project.jsx
+++ src/container/Projects/project.jsx
@@ -1,58 +1,61 @@
-import React , { useEffect , useState } from 'react'
-import styles from './project.module.css'
-import { adminInstance } from '../../axios'
-import Spinner from '../../components/UI/Spinner/spinner'
+import React, { useEffect, useState } from "react";
+import styles from "./project.module.css";
+import { adminInstance } from "../../axios";
+import Spinner from "../../components/UI/Spinner/spinner";
 
+export default function Project() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-export default function Project(){
-    const [loading,setLoading] = useState(true)
-    const [data,setData] = useState([])
+  useEffect(() => {
+    const abortController = new AbortController();
+    adminInstance
+      .get("/getProjects", { signal: abortController.signal })
+      .then((res) => {
+        setLoading(false);
+        setData(res.data);
+      })
+      .catch((err) => console.log(err));
 
-    useEffect(()=>{
-            const abortController = new AbortController();
-            adminInstance.get('/getProjects' , { signal : abortController.signal })
-            .then(res=>{
-                setLoading(false)
-                setData(res.data)
-            })
-            .catch(err=>console.log(err))
+    return () => {
+      abortController.abort();
+    };
+  }, []);
 
-            return () => {
-                abortController.abort();
-              };
-    },[])
-
-    return(
-        loading ? <Spinner /> :
-        <div className={styles.Container} id="Projects">
-            <div className={styles.Heading}>
-                <h1>Projects</h1>
+  return loading ? (
+    <Spinner />
+  ) : (
+    <div className={styles.Container} id="Projects">
+      <div className={styles.Heading}>
+        <h1>Projects</h1>
+      </div>
+      <div className={styles.container}>
+        {data.map((item) => {
+          return (
+            <div className={styles.card} key={item._id}>
+              <div className={styles.box}>
+                <div className={styles.content}>
+                  <img
+                    src={`https://github.com/AleemIlyas/Portfolio-backend/tree/main/Images/${item.image}`}
+                    alt={item.title}
+                    loading="lazy"
+                  />
+                  <h3> {item.title} </h3>
+                  <br />
+                  <p>{item.description.slice(0, 150)}</p>
+                  <a href={item.link} target="_blank">
+                    Go To App
+                  </a>
+                  <ul className={styles.Technologies}>
+                    {item.technologies.map((tech) => {
+                      return <li key={tech}> {tech} </li>;
+                    })}
+                  </ul>
+                </div>
+              </div>
             </div>
-            <div className={styles.container}>
-                {
-                    data.map( item=>{
-                       return(
-                        <div className={styles.card} key={item._id} >
-                        <div className={styles.box}>
-                            <div className={styles.content}>
-                                <img src={`http://localhost:5500/Images/${item.image}`} alt={item.title} loading='lazy' />
-                                <h3> {item.title} </h3><br/> 
-                                <p>{item.description.slice(0,150)}</p>
-                              <a href={item.link} target="_blank" >Go To App</a>
-                              <ul className={styles.Technologies}>
-                                {
-                                    item.technologies.map(tech=>{
-                                       return <li key={tech}> {tech} </li>
-                                    })
-                                }
-                              </ul>
-                            </div>
-                        </div>
-                    </div> )
-                    })
-                }
-
-      
+          );
+        })}
 
         {/* <div className={styles.card}>
             <div className={styles.box}>
@@ -75,8 +78,7 @@ export default function Project(){
                 </div>
             </div>
         </div> */}
-
+      </div>
     </div>
-</div>
-    )
+  );
 }
